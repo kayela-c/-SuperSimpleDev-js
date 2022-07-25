12bdbe6fd2ce7ddb32d0600477de3420e7a0dc56
kayela-c <kayela@icloud.com>
2022-07-24 20:32:57 -0500

    new files

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let todo1 = 'Get groceries';
+let todo2 = 'Wash car';
+let todo3 = 'Make Dinner';
+
+
+let element = document.createElement('div');
+element.innerText = todo1;
+document.body.appendChild(element);
+
+let element = document.createElement('div');
+element.innerText = todo2;
+document.body.appendChild(element);
